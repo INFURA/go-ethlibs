88e6af31fa1e2eab2c060e576f644df2c025f6f0
Ryan Schneider <ryanleeschneider@gmail.com>
2019-04-05 15:19:59 -0700

    eth: Add a TransactionReceipt object

---
+++ pkg/eth/transaction_receipt.go
@@ -0,0 +1,17 @@
+package eth
+
+type TransactionReceipt struct {
+	TransactionHash   Hash      `json:"transactionHash"`
+	TransactionIndex  Quantity  `json:"transactionIndex"`
+	BlockHash         Hash      `json:"blockHash"`
+	BlockNumber       Quantity  `json:"blockNumber"`
+	From              Address   `json:"from"`
+	To                *Address  `json:"to"`
+	CumulativeGasUsed Quantity  `json:"cumulativeGasUsed"`
+	GasUsed           Quantity  `json:"gasUsed"`
+	ContractAddress   *Address  `json:"contractAddress"`
+	Logs              []Log     `json:"logs"`
+	LogsBloom         Data256   `json:"logsBloom"`
+	Root              *Data32   `json:"root,omitempty"`
+	Status            *Quantity `json:"status,omitempty"`
+}
