2b87736c3b594186ac7f9acc5c5dfd7b6259cca0
Ryan Schneider <ryanleeschneider@gmail.com>
2019-05-15 11:45:52 -0700

    eth: Improve test coverage

--- pkg/eth/data_test.go
+++ pkg/eth/data_test.go
@@ -1,6 +1,7 @@
 package eth_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -23,4 +24,45 @@ func TestData(t *testing.T) {
 
 	_, err = eth.NewData("0xfoodbarr")
 	require.Error(t, err)
+
+	_, err = eth.NewData("badf00d")
+	require.Error(t, err)
+
+	{
+		d, err := eth.NewData("0x")
+		require.NoError(t, err)
+		require.Equal(t, "0x", d.String())
+	}
+
+	{
+		d, err := eth.NewData8("0x1122334455667788")
+		require.NoError(t, err)
+		require.NotNil(t, d)
+	}
+
+	{
+		d, err := eth.NewData20("0x1122334455667788990011223344556677889900")
+		require.NoError(t, err)
+		require.NotNil(t, d)
+	}
+
+	{
+		d, err := eth.NewData32("0x1122334455667788990011223344556677889900112233445566778899001122")
+		require.NoError(t, err)
+		require.NotNil(t, d)
+
+		h, err := eth.NewHash(d.String())
+		require.NoError(t, err)
+		require.NotNil(t, h)
+
+		tt, err := eth.NewTopic(d.String())
+		require.NoError(t, err)
+		require.NotNil(t, tt)
+	}
+
+	{
+		d, err := eth.NewData256("0x" + strings.Repeat("00", 256))
+		require.NoError(t, err)
+		require.NotNil(t, d)
+	}
 }
--- pkg/eth/logs_test.go
+++ pkg/eth/logs_test.go
@@ -14,6 +14,7 @@ func TestLogFilterParsing(t *testing.T) {
 		Message  string
 		Payload  string
 		Expected eth.LogFilter
+		Error    bool
 	}
 
 	tests := []TestCase{
@@ -248,15 +249,30 @@ func TestLogFilterParsing(t *testing.T) {
 				},
 			},
 		},
+		{
+			Message:  "invalid topics should fail",
+			Payload:  `{"topics":["0xfoo4foo4foo4foo4foo4foo4foo4foo4foo4foo4foo4foo4foo4foo4foo4foo4"]}`,
+			Expected: eth.LogFilter{},
+			Error:    true,
+		},
+		{
+			Message:  "invalid topic array should fail",
+			Payload:  `{"topics":[["0xfoo4foo4foo4foo4foo4foo4foo4foo4foo4foo4foo4foo4foo4foo4foo4foo4"]]}`,
+			Expected: eth.LogFilter{},
+			Error:    true,
+		},
 	}
 
 	for _, test := range tests {
 		actual := eth.LogFilter{}
 		err := json.Unmarshal([]byte(test.Payload), &actual)
-		require.NoError(t, err, test.Message)
-		require.Equal(t, test.Expected, actual, test.Message)
+		if test.Error {
+			require.Error(t, err, test.Message)
+		} else {
+			require.NoError(t, err, test.Message)
+			require.Equal(t, test.Expected, actual, test.Message)
+		}
 	}
-
 }
 
 func TestLog_UnmarshalJSON(t *testing.T) {
