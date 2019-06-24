d287e53e269b800fd9739b0df84e329cebb5737a
Andrey Petrov <andrey.petrov@shazow.net>
2019-06-24 13:50:33 -0400

    ethlibs: Add godoc

---
+++ godoc.go
@@ -0,0 +1,10 @@
+/*
+go-ethlibs: Go Ethereum Libraries
+
+A sandbox of helpers related to interacting with Ethereum nodes.
+
+We hope code snippets in this repository will come in useful for others, but
+please don't expect any stability guarantees. We'd like for the best pieces to
+eventually graduate into stand-alone packages.
+*/
+package ethlibs
