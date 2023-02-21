04d006be183aae1aabf239c94fd78485b1c7e603
Ryan Schneider <ryanleeschneider@gmail.com>
2023-02-21 07:39:30 -0800

    eth.Block: add support for EIP-4895 withdrawals (#76)
    * eth.Block: add support for EIP-4895 withdrawals
    * notably using `withdrawal.address` instead of `.recipient` to match real world EL behavior despite being "out of spec"
    
    ---------
    
    Co-authored-by: akuanti <afriyie.nti@gmail.com>

---
+++ eth/withdrawals.go
@@ -0,0 +1,23 @@
+package eth
+
+import "github.com/INFURA/go-ethlibs/rlp"
+
+type Withdrawal struct {
+	Index          Quantity `json:"index"`
+	ValidatorIndex Quantity `json:"validatorIndex"`
+	Address        Address  `json:"address"`
+	Amount         Quantity `json:"amount"`
+}
+
+// RLP returns the EIP-4895 RLP encoding of a Withdrawal item
+func (w *Withdrawal) RLP() rlp.Value {
+	// withdrawal_0 = [index_0, validator_index_0, address_0, amount_0]
+	return rlp.Value{
+		List: []rlp.Value{
+			w.Index.RLP(),
+			w.ValidatorIndex.RLP(),
+			w.Address.RLP(),
+			w.Amount.RLP(),
+		},
+	}
+}
