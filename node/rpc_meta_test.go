5196605f2cd88747e16d18617d98ec02e47f7229
canonbrother <w.canonbrother@gmail.com>
2023-05-25 10:41:25 +0800

    wip

---
+++ node/rpc_meta_test.go
@@ -0,0 +1,227 @@
+package node_test
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/INFURA/go-ethlibs/eth"
+	"github.com/INFURA/go-ethlibs/node"
+)
+
+// const ALICE = "0x4b73afe4bdba1ef40025d9002da78ca6a09d56b5"
+const ALICE = "0x4b73AFe4BDba1Ef40025D9002da78ca6a09d56b5"
+
+func getMetaDevClient(t *testing.T, ctx context.Context) node.Client {
+	url := "http://35.187.53.161:20551/"
+
+	// Create connection
+	conn, err := node.NewClient(ctx, url, http.Header{})
+	require.NoError(t, err, "creating connection should not fail")
+	return conn
+}
+
+// func TestMetaRpc_Call_Simple_Contract(t *testing.T) {
+// 	ctx := context.Background()
+// 	conn := getMetaDevClient(t, ctx)
+
+// 	tx := eth.Transaction{
+// 		From:  *eth.MustAddress("0x148772F29058DcC772613260b078dCa8C14afF6c"),
+// 		To:    eth.MustAddress("0x790b2DaF786774BEe346b9B8913b259eD2d354D0"), // contract address
+// 		Value: *eth.MustQuantity("0x00"),
+// 		// mul(2,3)
+// 		Input: *eth.MustData("0xc8a4ac9c00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000003"),
+// 	}
+
+// 	hash, err := conn.Call(ctx, tx, *eth.MustBlockNumberOrTag("latest"))
+// 	require.NoError(t, err)
+// 	require.Equal(t, hash, "0x0000000000000000000000000000000000000000000000000000000000000006")
+// }
+
+func TestMetaRpc_Get_Accounts(t *testing.T) {
+	ctx := context.Background()
+	conn := getMetaDevClient(t, ctx)
+
+	accountList, err := conn.GetAccounts(ctx)
+	println("accountList: ", accountList)
+	require.NoError(t, err)
+	require.Equal(t, accountList[0], eth.Address(ALICE))
+}
+
+func TestMetaRpc_Get_Balance(t *testing.T) {
+	ctx := context.Background()
+	conn := getMetaDevClient(t, ctx)
+
+	bal, err := conn.GetBalance(ctx, *eth.MustAddress(ALICE), *eth.MustBlockNumberOrTag("latest"))
+	require.NoError(t, err)
+	require.NotNil(t, bal)
+}
+
+// todo
+// func TestMetaRpc_GetTransactionCount(t *testing.T) {
+// 	ctx := context.Background()
+// 	conn := getMetaDevClient(t, ctx)
+
+// 	// Checks the current pending nonce for account can be retrieved
+// 	blockNum1 := eth.MustBlockNumberOrTag("latest")
+// 	pendingNonce1, err := conn.GetTransactionCount(ctx, "0xed28874e52A12f0D42118653B0FBCee0ACFadC00", *blockNum1)
+// 	require.NoError(t, err)
+// 	require.NotEmpty(t, pendingNonce1, "pending nonce must not be nil")
+
+// 	// Should catch failure since it is looking for a nonce of a future block
+// 	blockNum2 := eth.MustBlockNumberOrTag("0x7654321")
+// 	pendingNonce2, err := conn.GetTransactionCount(ctx, "0xed28874e52A12f0D42118653B0FBCee0ACFadC00", *blockNum2)
+// 	require.Error(t, err)
+// 	require.Empty(t, pendingNonce2, "pending nonce must not exist since it is a future block")
+// }
+
+// error
+// func TestMetaRpc_EstimateGas(t *testing.T) {
+// 	ctx := context.Background()
+// 	conn := getMetaDevClient(t, ctx)
+
+// 	from := eth.MustAddress(ALICE)
+// 	tx := eth.Transaction{
+// 		Nonce:    eth.QuantityFromUInt64(146),
+// 		GasPrice: eth.OptionalQuantityFromInt(3000000000),
+// 		Gas:      eth.QuantityFromUInt64(22000),
+// 		To:       eth.MustAddress("0x43700db832E9Ac990D36d6279A846608643c904E"),
+// 		Value:    *eth.OptionalQuantityFromInt(100),
+// 		From:     *from,
+// 	}
+
+// 	gas, err := conn.EstimateGas(ctx, tx)
+// 	require.NoError(t, err)
+// 	require.NotEqual(t, gas, 0, "estimate gas cannot be equal to zero.")
+// }
+
+// error
+// func TestMetaRpc_MaxPriorityFeePerGas(t *testing.T) {
+// 	ctx := context.Background()
+// 	conn := getMetaDevClient(t, ctx)
+
+// 	fee, err := conn.MaxPriorityFeePerGas(ctx)
+// 	println("fee: ", fee)
+// 	require.NoError(t, err)
+// 	// require.NotEqual(t, fee, 0, "fee cannot be equal to 0")
+// }
+
+func TestMetaRpc_GasPrice(t *testing.T) {
+	ctx := context.Background()
+	conn := getMetaDevClient(t, ctx)
+
+	gasPrice, err := conn.GasPrice(ctx)
+	require.NoError(t, err)
+	require.NotEqual(t, gasPrice, 0, "gas price cannot be equal to 0")
+	gasP, _ := strconv.ParseUint("0x2540be400", 10, 64)
+	println("gasP: ", gasP)
+	// require.Equal(t, gasPrice, uint64("0x2540be400")) // 10_000_000_000
+}
+
+func TestMetaRpc_NetVersion(t *testing.T) {
+	ctx := context.Background()
+	conn := getMetaDevClient(t, ctx)
+
+	netVersion, err := conn.NetVersion(ctx)
+	require.NoError(t, err)
+	require.NotEmpty(t, netVersion, "net version id must not be nil")
+	require.Equal(t, netVersion, "1132")
+}
+
+// func TestMetaRpc_ChainId(t *testing.T) {
+// 	ctx := context.Background()
+// 	conn := getMetaDevClient(t, ctx)
+
+// 	chainId, err := conn.ChainId(ctx)
+// 	require.NoError(t, err)
+// 	require.NotEmpty(t, chainId, "chain id must not be nil")
+// }
+
+// func TestMetaRpc_SendRawTransactionInValidEmpty(t *testing.T) {
+// 	ctx := context.Background()
+// 	conn := getMetaDevClient(t, ctx)
+
+// 	txHash, err := conn.SendRawTransaction(ctx, "0x0")
+// 	require.Error(t, err)
+// 	require.Empty(t, txHash, "txHash must be nil")
+// }
+
+// func TestMetaRpc_SendRawTransactionInValidOldNonce(t *testing.T) {
+// 	ctx := context.Background()
+// 	conn := getMetaDevClient(t, ctx)
+
+// 	data := eth.MustData("0xf86e0185174876e8008252089460c063d3f3b744e2d153fcbe66a068b09109cf1b865af3107a400084baadf00d2ea0b4d9e2edbd2a2d9a38cf0415f9d03849e6a6f2de8562d7cd74eda89397882030a056edb455e9ffa07ad22f8b06f9065564911f796a026e1b2177ecaad995198aaa")
+// 	txHash, err := conn.SendRawTransaction(ctx, data.String())
+// 	require.Error(t, err)
+// 	require.Equal(t, err.Error(), "{\"code\":-32000,\"message\":\"nonce too low\"}")
+// 	require.Empty(t, txHash, "txHash must be nil")
+// }
+
+// func TestMetaRpc_FutureBlockByNumber(t *testing.T) {
+// 	ctx := context.Background()
+// 	conn := getMetaDevClient(t, ctx)
+
+// 	blockNumber, err := conn.BlockNumber(ctx)
+// 	require.NoError(t, err)
+
+// 	next, err := conn.BlockByNumber(ctx, blockNumber+1000, false)
+// 	require.Nil(t, next, "future block should be nil")
+// 	require.Error(t, err, "requesting a future block should return an error")
+// 	require.Equal(t, node.ErrBlockNotFound, err)
+
+// 	// get a the genesis block by number which should _not_ fail
+// 	genesis, err := conn.BlockByNumber(ctx, 0, false)
+// 	require.NoError(t, err, "requesting genesis block by number should not fail")
+// 	require.NotNil(t, genesis, "genesis block must not be nil")
+// }
+
+// func TestMetaRpc_InvalidBlockByHash(t *testing.T) {
+// 	ctx := context.Background()
+// 	conn := getMetaDevClient(t, ctx)
+
+// 	b, err := conn.BlockByHash(ctx, "invalid", false)
+// 	require.Error(t, err, "requesting an invalid hash should return an error")
+// 	require.Nil(t, b, "block from invalid hash should be nil")
+
+// 	b, err = conn.BlockByHash(ctx, "0x1234", false)
+// 	require.Error(t, err, "requesting an invalid hash should return an error")
+// 	require.Nil(t, b, "block from invalid hash should be nil")
+
+// 	b, err = conn.BlockByHash(ctx, "0x0badf00dbadf00dbadf00dbadf00dbadf00dbadf00dbadf00dbadf00dbadf00d", false)
+// 	require.Error(t, err, "requesting a non-existent block should should return an error")
+// 	require.Nil(t, b, "block from non-existent hash should be nil")
+// 	require.Equal(t, node.ErrBlockNotFound, err)
+
+// 	// get the genesis block which should _not_ fail
+// 	// https://goerli.etherscan.io/block/8000000
+// 	b, err = conn.BlockByHash(ctx, "0x2ae83825ac6b2a2b2509da8617cf31072a5628e9a818f177316f4f4bcdfafd06", true)
+// 	require.NoError(t, err, "genesis block hash should not return an error")
+// 	require.NotNil(t, b, "genesis block should be retrievable by hash")
+// }
+
+// func TestMetaRpc_InvalidTransactionByHash(t *testing.T) {
+// 	ctx := context.Background()
+// 	conn := getMetaDevClient(t, ctx)
+
+// 	tx, err := conn.TransactionByHash(ctx, "invalid")
+// 	require.Error(t, err, "requesting an invalid hash should return an error")
+// 	require.Nil(t, tx, "tx from invalid hash should be nil")
+
+// 	tx, err = conn.TransactionByHash(ctx, "0x1234")
+// 	require.Error(t, err, "requesting an invalid hash should return an error")
+// 	require.Nil(t, tx, "tx from invalid hash should be nil")
+
+// 	tx, err = conn.TransactionByHash(ctx, "0x0badf00dbadf00dbadf00dbadf00dbadf00dbadf00dbadf00dbadf00dbadf00d")
+// 	require.Error(t, err, "requesting an non-existent hash should return an error")
+// 	require.Nil(t, tx, "tx from non-existent hash should be nil")
+// 	require.Equal(t, node.ErrTransactionNotFound, err)
+
+// 	// get an early tx which should _not_ fail
+// 	// https://goerli.etherscan.io/tx/0x752ca2e3175c0dfd8b8612abcd2dac3134445f29e764d33645726cbcd57aefd1
+// 	tx, err = conn.TransactionByHash(ctx, "0x752ca2e3175c0dfd8b8612abcd2dac3134445f29e764d33645726cbcd57aefd1")
+// 	require.NoError(t, err, "early tx should not return an error")
+// 	require.NotNil(t, tx, "early tx should be retrievable by hash")
+// }
